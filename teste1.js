c5ed7fed9e8fad85d80ecab55dfb97fa1e5a4343
chrixtianny <christianny@live.com>
2024-02-23 17:53:18 -0300

    1st

---
+++ teste1.js
@@ -0,0 +1,14 @@
+let indice;
+let soma;
+let k;
+
+indice = parseInt(13);
+soma = parseInt(0);
+k = parseInt(0);
+
+while (k < indice) {
+  k = k + 1;
+  soma = soma + k;
+}
+
+console.log(soma);
---
+++ teste1.py
@@ -0,0 +1,9 @@
+indice = int(13)
+soma = int(0)
+k = int(0)
+
+while k < indice:
+  k = k + 1
+  soma = soma + k
+
+print(soma)
